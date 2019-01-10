fbbbbd92291f97f039ec15a0fa9e5b2a13f1db64
Zach Feldman <maisaimamovic@gmail.com>
2019-01-10 15:32:41 +0100

    So much commitment

--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -1,20 +1,27 @@
 $(document).ready(() => {
+let start = Date.now();
+  
     $('#this').keyup(() => {
-        let inputValue = $('#this').val();
-        console.log('This is :' + inputValue);
+        let inputValue = $('#this').val().toLocaleLowerCase();
         
+        if ((Date.now() - start) > 300) {
+           console.log("This is it:" + inputValue);
+
         $.ajax({
             type: 'POST',
             url: '/ajax',
             data: {
                 name : inputValue
             },
+            
         }).done((response) => {
             $('#listOfNames').empty();
             for(let i in response) {
                 $('#listOfNames').append('<li>' + response[i].firstname + response[i].lastname + '</li');
             }
         });
+        }
     });
 
 });
+
